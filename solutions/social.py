f8267a726b4b2b13fd9d7f780e8ae040d5e051f7
hreinstein <59658346+hreinstein@users.noreply.github.com>
2020-03-18 14:34:19 -0700

    Update social.py

--- solutions/social.py
+++ solutions/social.py
@@ -51,13 +51,20 @@ if __name__ == "__main__":
     #
     # "Print" the screen name of the user who authored the first tweet (i.e. "sandwhoa"):
 
-
+print("Question A:")
+print(tweets[0]["user"]["screen_name"])
     #
     # QUESTION B
     #
     # Of all the tweets which include the phrase "@sandwhoa" in their full text,
     # ... "print" the screen name of the user who authored that tweet,
     # ... each on a separate line (i.e. "person2", then "person3"):
+
+for tweet in tweets: 
+    if "@sandwhoa" in tweet["ull_text"]:
+        print(tweet["user"]["screen_name"])
+
+
 
 
     #
@@ -68,3 +75,36 @@ if __name__ == "__main__":
     # ... and then "print" the screen name of the user who authored that tweet (i.e. "person3").
     # ... FYI: Assume the tweet order can change at any time
     # ... and has no relationship with the number of likes.
+
+
+
+##class one 
+#
+## checking to see if the Git utility is installed
+#git --version
+## cloning a repo from the command-line:
+#cd ~/Desktop/
+#git clone git@github.com:s2t2/exam-solutions-starter-py.git # OR YOUR OWN REMOTE ADDRESS
+#cd exam-solutions-starter-py/
+#code .
+#
+## running python applications from the "base" anaconda env:
+#python solutions/social.py
+#
+## committing on the "master" branch / default work space:
+#git branch # make sure you're on the master branch
+## make some changes via text editor
+#git status # which files have changed?
+#git diff # how have the files changed?
+#git add . # prepare all the files to be committed
+#git commit -m "Example commit" # save / commit a new version locally
+## after one or more local commits, push to GitHub:
+#git remote -v # checking to see which remote assiciations, for example "origin" points to repo on GitHub
+#git push origin master # upload / push code to your GitHub repo's master branch
+#
+## branch operations / committing in a different working space:
+#git checkout -b tweets # create a new branch / working space called "tweets" and activate it
+#git branch # check that the tweets branch is active
+## repeat the local process to change files and make a commit, then...
+#git push origin tweets # to push to the "tweets" branch on GitHub
+## then visit your repo on GitHub and open up a new "Pull Request" for the specified branch, where you can review 
